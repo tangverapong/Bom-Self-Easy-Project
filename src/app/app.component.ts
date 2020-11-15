9ca8c1afcb5d9a2a7aa9b6a8ca7317073dd9cd98
Akegapon Tangverapong <akegapon.tangverapong@ses.com>
2020-11-15 21:37:57 +0100

    Change test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  name = 'Bom ' + VERSION.major;
 }
